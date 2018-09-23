12987cfea0c6e4d6d714e112acbeaf768812cef2
Slawomir Czerwinski <slawomir@czerwinski.it>
2018-09-23 18:46:42 +0100

    Empty pie chart

---
+++ piechart/src/main/java/it/czerwinski/android/charts/piechart/PieChart.kt
@@ -0,0 +1,11 @@
+package it.czerwinski.android.charts.piechart
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PieChart @JvmOverloads constructor(
+    context: Context?,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.pieChartStyle
+) : View(context, attrs, defStyleAttr)
