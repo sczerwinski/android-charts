200fa162af1d128bc4a55932f24a9848a0b9dfc4
Slawomir Czerwinski <slawomir@czerwinski.it>
2018-09-23 21:13:15 +0100

    Fixing math error

--- piechart/src/main/java/it/czerwinski/android/charts/common/Math.kt
+++ piechart/src/main/java/it/czerwinski/android/charts/common/Math.kt
@@ -5,8 +5,8 @@ internal const val DOUBLE_PI: Float = PI * 2f
 
 internal const val FULL_ANGLE: Float = 360f
 
-internal fun Float.radToDeg(): Float = this * DOUBLE_PI / FULL_ANGLE
-internal fun Float.degToRad(): Float = this * FULL_ANGLE / DOUBLE_PI
+internal fun Float.radToDeg(): Float = this * FULL_ANGLE / DOUBLE_PI
+internal fun Float.degToRad(): Float = this * DOUBLE_PI / FULL_ANGLE
 
 internal fun Iterable<Float>.partialSums(): List<Float> =
     fold(listOf(0f)) { sums, next ->
